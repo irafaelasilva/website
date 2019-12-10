3190a3fcef21cc1406b1e90e4b5d860e4471fca0
Rafaela <rafalucy3@gmail.com>
2019-12-10 21:22:04 +0000

    Add styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hey ya!</h1>
+      <h2>I'm Rafaela Silva, a software developer based in London</h2>
+    </div>
+  )
+}
+
+export default IndexPage
