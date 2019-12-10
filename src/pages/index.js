4e58bcf1c880cfed1d439d63e391fd31c1d69e72
Rafaela <rafalucy3@gmail.com>
2019-12-10 21:45:47 +0000

    Add style and content at index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,53 @@
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100%;
+`
+
+const Title = styled.h1`
+  font-weight: lighter;
+`
+
+const SubTitle = styled.h4`
+  font-weight: lighter;
+`
+
+const Name = styled.span`
+  font-weight: normal;
+`
+
+const Company = styled.a`
+  color: inherit;
+  text-decoration: none;
+  font-weight: normal;
+`
 
 const IndexPage = () => {
   return (
-    <div>
-      <h1>Hey ya!</h1>
-      <h2>I'm Rafaela Silva, a software developer based in London</h2>
-    </div>
+    <Container>
+      <Title>
+        Hey <span>👋</span>, my name is <Name>Rafaela</Name>
+      </Title>
+      <br />
+      <SubTitle>
+        I'm a Software Enginner, just finished a bootcamp at{" "}
+        <Company
+          href="https://makers.tech/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Makers Academy
+        </Company>
+      </SubTitle>
+      <br />
+      <SubTitle>🇧🇷🇬🇧</SubTitle>
+    </Container>
   )
 }
 
